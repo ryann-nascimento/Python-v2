e505c6d049f292138126d03c03da9cc142f2db34
Ryann Nascimento <ryanncarvalho21@gmail.com>
2026-05-21 23:26:04 -0300

    Meu primeiro Script
    Esse foi o meu primeiro script no GitHub

---
+++ primeiro_script.py
@@ -0,0 +1,14 @@
+x = input("Digite um número: ")
+y = input("Digite outro número: ")
+
+soma = float(x) + float(y)
+print("A soma dos números é:", soma)
+
+subtracao = float(x) - float(y)
+print("A subtração dos números é:", subtracao)
+
+multiplicacao = float(x) * float(y)
+print("A multiplicação dos números é:", multiplicacao)
+
+divisao = float(x) / float(y)
+print("A divisão dos números é:", divisao)
